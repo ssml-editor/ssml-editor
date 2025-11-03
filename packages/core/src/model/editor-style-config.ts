e280cc0fa249911590ec2458ab08e2897e64bf38
hz <beijingren@vip.qq.com>
2025-11-03 11:27:59 +0800

    修改 EditorStyleConfig 模型继承 Properties 和 PropertiesHyphen，并增加 maxHeight 属性，并修改 height 和 minHeight 属性的类型

--- packages/core/src/model/editor-style-config.ts
+++ packages/core/src/model/editor-style-config.ts
@@ -1,6 +1,9 @@
-import type { Property } from 'csstype';
+import type { Properties, PropertiesHyphen, Property } from 'csstype';
 
-export interface EditorStyleConfig {
-  height?: Property.Height<string>;
-  minHeight?: Property.MinHeight<string>;
+export interface EditorStyleConfig
+  extends Properties<string | number>,
+    PropertiesHyphen<string | number> {
+  height?: Property.Height<string | number>;
+  minHeight?: Property.MinHeight<string | number>;
+  maxHeight?: Property.MaxHeight<string | number>;
 }
